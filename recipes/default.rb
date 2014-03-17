5fa70cbc824b5d1c7ed8f8d558f52f74c33d721f
Ted Chen <ted@nephilagraphic.com>
2014-03-16 23:50:20 -0300

    Creates l2tp/ipsec configuration files.

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,3 +17,88 @@
 # limitations under the License.
 #
 
+#find_public_ip = Mixlib::ShellOut.new("ifconfig #{node['vpn-server']['public_interface']} | grep \"inet addr\" | awk 'BEGIN{FS=\":\"}{print $2}' | awk '{print $1}'")
+#find_private_ip = Mixlib::ShellOut.new("ifconfig #{node['vpn-server']['private_interface']} | grep \"inet addr\" | awk 'BEGIN{FS=\":\"}{print $2}' | awk '{print $1}'")
+
+#find_public_ip.run_command
+#find_private_ip.run_command
+
+
+#node.default['vpn-server']['public_ip'] = find_public_ip.stdout.strip
+#node.default['vpn-server']['private_ip'] = find_private_ip.stdout.strip
+#
+
+%w(lsof ppp xl2tpd openswan).each do |p|
+  package p
+end
+
+# Service definitions
+#
+service 'xl2tpd' do
+  supports :status => true, :restart => true, :start => true, :stop => true
+  action :nothing
+end
+
+service 'ipsec' do
+  supports :status => true, :restart => true, :start => true, :stop => true
+  action :nothing
+end
+
+
+template '/etc/ipsec.conf' do
+  source 'ipsec.conf.erb'
+  owner   'root'
+  group   'root'
+  mode    0644
+
+  variables(
+      :ppp_link_network => node['l2tp-ipsec']['ppp_link_network'],
+      :public_ip => node['l2tp-ipsec']['public_ip'],
+      :private_ip => node['l2tp-ipsec']['private_ip']
+  )
+
+  notifies :restart, 'service[ipsec]', :delayed
+end
+
+
+template '/etc/ipsec.secrets' do
+  source  'ipsec.secrets.erb'
+  owner   'root'
+  group   'root'
+  mode    0600
+
+  variables(
+      :public_ip => node['l2tp-ipsec']['public_ip'],
+      :preshared_key => node['l2tp-ipsec']['preshared_key']
+  )
+  notifies :restart, 'service[ipsec]', :delayed
+end
+
+
+template "#{node['l2tp-ipsec']['ppp_path']}/chap-secrets" do
+  source 'chap-secrets.erb'
+  variables(
+      :users => node['l2tp-ipsec']['users']
+  )
+  notifies :restart, 'service[xl2tpd]', :delayed
+  notifies :restart, 'service[ipsec]', :delayed
+end
+
+
+template "#{node['l2tp-ipsec']['xl2tpd_path']}/xl2tpd.conf" do
+  source 'xl2tpd.conf.erb'
+  variables(
+      :virtual_ip_range => node['l2tp-ipsec']['virtual_ip_range'],
+      :virtual_interface_ip => node['l2tp-ipsec']['virtual_interface_ip'],
+      :pppoptfile => node['l2tp-ipsec']['pppoptfile']
+  )
+  notifies :restart, 'service[xl2tpd]', :delayed
+end
+
+template node['l2tp-ipsec']['pppoptfile'] do
+  source 'options.xl2tpd.erb'
+  variables(
+      :dns_servers => node['l2tp-ipsec']['dns_servers']
+  )
+  notifies :restart, 'service[xl2tpd]', :delayed
+end
