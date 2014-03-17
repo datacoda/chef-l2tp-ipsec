4b4446992906bc9d61ae39ebd9bfd8573f81e65d
Ted Chen <ted@nephilagraphic.com>
2014-03-16 21:35:43 -0300

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'l2tp-ipsec'
+maintainer       'Nephila Graphic'
+maintainer_email 'ted@nephilagraphic.com'
+license          'All rights reserved'
+description      'Installs/Configures l2tp-ipsec'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.0.1'
+
