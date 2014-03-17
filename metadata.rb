017ba9a40a3ea44801b6855e4e1b4608cc6a09bc
Ted Chen <ted@nephilagraphic.com>
2014-03-17 01:04:01 -0300

    Updated license

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'l2tp-ipsec'
 maintainer       'Nephila Graphic'
 maintainer_email 'ted@nephilagraphic.com'
-license          'All rights reserved'
+license          'Apache 2.0'
 description      'Installs/Configures l2tp-ipsec'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.0.1'
