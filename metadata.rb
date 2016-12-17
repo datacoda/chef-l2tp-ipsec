18f5c4c5b55e98ca3cf55f37d421aaaac75c94af
Ted Chen <ted@nephilagraphic.com>
2016-12-17 19:00:28 -0400

    Added Gemfile.lock Updated kitchen test boxes Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'datacoda@gmail.com'
 license 'Apache 2.0'
 description 'Installs/Configures l2tp-ipsec'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '0.2.0'
 source_url 'https://github.com/datacoda/chef-l2tp-ipsec'
 issues_url 'https://github.com/datacoda/chef-l2tp-ipsec/issues'
 
